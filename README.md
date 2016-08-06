93b88daadad7ef4752ca7877b4f1bbf2ab5c07fb
subdavis <bdavis@redspin.net>
2016-08-06 01:19:29 -0400

    Initial project code

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # registration
-A flask app for registration.
+A flask app for HackNC registration using MyMLH for auth.
+
+# Development
+```
+git clone https://github.com/HackNC/registration
+cd registration
+virtualenv -p python3 .
+source bin/activate
+pip install -e .
+python registration.py
+```
+And you're up and running!
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='registration',
+      version='0.1',
+      description='HackNC Registration app',
+      author='Brandon Davis',
+      author_email='bdavis@redspin.net',
+      url='https://hacknc.com',
+      install_requires=[
+        'flask==0.11.1',
+        'requests==2.10.0',
+        'requests_oauthlib==0.6.2'],
+)
